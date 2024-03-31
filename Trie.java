083dcaa8a447cf1203dc6dace858d77af4ea30f6
jinkimaz <143664962+jinkimaz@users.noreply.github.com>
2024-03-30 20:56:33 -0700

    Implementing search
    In the process of implementing search

--- Trie.java
+++ Trie.java
@@ -1,3 +1,16 @@
 public class Trie {
+
+    // search returns true if word is in trie
+    public boolean search(String word) {
+        TrieNode cur = root;
+
+        // only iterate for the length of the word
+        // search would take O(n) time, where n is the length of the word
+        for (int i = 0; i < word.length(); i++) {
+            // index into the children to achieve O(1) time
+        }
+
+        return true;
+    }
     
 }
