36169782b2ab32b3b40e65f865c1bef9bb29b860
Abrorjon Asralov <asralov@arizona.edu>
2024-03-31 21:04:35 -0700

    update
    Worked on two classes for Trie and Trie Node and implemented the constructors

--- Trie.java
+++ Trie.java
@@ -1,8 +1,21 @@
-public class Trie {
+
+
+public class Trie 
+{
+    private TrieNode[] root;
+    public Trie()
+    {
+        // first having a root that has 26 possible characters
+        // but initially the array consists of all nulls
+        // we can add that as we add characters and traverse 
+        // through them
+        root = new TrieNode[26];
+    }
 
     // search returns true if word is in trie
     public boolean search(String word) {
-        TrieNode cur = root;
+        // changed TrieNode cur = root; to be TrieNode[] so we can acrtually iterate through that
+        TrieNode[] cur = root;
 
         // only iterate for the length of the word
         // search would take O(n) time, where n is the length of the word
--- TrieNode.java
+++ TrieNode.java
@@ -1,3 +1,38 @@
-public class TrieNode {
+/**
+ * This is a TrieNode class
+ */
+public class TrieNode 
+{
+    // private attributes
+    private char letter; // letter of from the word
+    private TrieNode[] children; // other possible words those are TrieNodes in array
+    private int numOfChildren = 0; // for keeping the count for indexing array
+
+    // Constructor
+    public TrieNode(char character)
+    {
+        this.letter = character;
+        this.children = new TrieNode[26]; // 26 possible combos with letters
+    }
+
+    /**
+     * This is a getter method that returns this TrieNode's character that
+     * is a letter for the possible word
+     * @return char
+     */
+    public char letter()
+    {
+        return this.letter;
+    }
+
+    /**
+     * This is a setter method that appends children collection of this TrieNode
+     * @param trieNode
+     */
+    public void appendChild(TrieNode trieNode)
+    {
+        children[numOfChildren] = trieNode;
+        numOfChildren++;
+    }
     
 }
