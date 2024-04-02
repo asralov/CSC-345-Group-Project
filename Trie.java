0a2e88bfce7a7e03f1091dc43beb06eb1c1dd3ec
jinkimaz <143664962+jinkimaz@users.noreply.github.com>
2024-04-01 19:29:26 -0700

    Implement search and add getters and setters
    Added boolean attribute isWord to determine if we've reached a "complete" word
    
    Trie would have a root (at the top-most level) with no character associated with it. Therefore I added another constructor TrieNode() which does not take any parameter.
    
    Added getter/setter methods for isWord and children because I wasn't able to access it from Trie.java.
    
    In Trie.java,  root = new TrieNode() will initialize a root TrieNode (which will have 26 possible children.)
    
    Implemented search assuming that we are doing a case insensitive search. In search(String word), I am changing word to lower case.
    
    Search begins by setting the cur pointer to the root node of Trie. It then recurses into its children.

--- Trie.java
+++ Trie.java
@@ -2,28 +2,41 @@
 
 public class Trie 
 {
-    private TrieNode[] root;
+    private TrieNode root;
     public Trie()
     {
         // first having a root that has 26 possible characters
         // but initially the array consists of all nulls
         // we can add that as we add characters and traverse 
         // through them
-        root = new TrieNode[26];
+
+        // the root of the Trie would not have any character associated with it
+        // root will still have children, which is array of 26 TrieNodes
+        root = new TrieNode();
     }
 
     // search returns true if word is in trie
+    // case insensitive search
     public boolean search(String word) {
-        // changed TrieNode cur = root; to be TrieNode[] so we can acrtually iterate through that
-        TrieNode[] cur = root;
+        TrieNode cur = root;
+        String wordLower = word.toLowerCase();
 
         // only iterate for the length of the word
         // search would take O(n) time, where n is the length of the word
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < wordLower.length(); i++) {
             // index into the children to achieve O(1) time
+            int index = wordLower.charAt(i) - 'a';
+            
+            if (cur.children()[index] == null) {
+                // character not found
+                return false;
+            } else {
+                // change cur pointer to the next TrieNode (next character)
+                cur = cur.children()[index];
+            }
         }
 
-        return true;
-    }
-    
+        // to determine if we've reached the end of the word
+        return cur.isWord();
+    }    
 }
--- TrieNode.java
+++ TrieNode.java
@@ -7,11 +7,18 @@ public class TrieNode
     private char letter; // letter of from the word
     private TrieNode[] children; // other possible words those are TrieNodes in array
     private int numOfChildren = 0; // for keeping the count for indexing array
+    private boolean isWord = false; // for checking if we've reached the end of the word
 
     // Constructor
     public TrieNode(char character)
     {
         this.letter = character;
+        this.children = new TrieNode[26]; // 26 possible combos with letters
+    }
+
+    // Constructor for root
+    public TrieNode()
+    {
         this.children = new TrieNode[26]; // 26 possible combos with letters
     }
 
@@ -33,6 +40,24 @@ public class TrieNode
     {
         children[numOfChildren] = trieNode;
         numOfChildren++;
+    }
+
+    // setter for isWord
+    public void setIsWord(boolean isWord)
+    {
+        this.isWord = isWord;
+    }
+
+    // getter for isWord
+    public boolean isWord()
+    {
+        return this.isWord;
+    }
+
+    // getter for children
+    public TrieNode[] children()
+    {
+        return this.children;
     }
     
 }
